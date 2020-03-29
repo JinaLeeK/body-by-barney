cf97c7b1e429860bcd77df9c4c96bd00539a0bbe
Kathryn DiPippo <dipippo.k@gmail.com>
2020-03-28 23:51:21 -0400

    Barney now works

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 const { Toolkit } = require('actions-toolkit')
-const { context, github: { request } } = new Toolkit()
-context.repo
-const pull_number = context.payload.pull_request.number;
-const barney = 'https://user-images.githubusercontent.com/1390106/66919899-3a406900-eff0-11e9-83ba-4e6c4c3dbfaf.jpg'
+const getPR = require('./lib/get-pr')
+const updatePR = require('./lib/update-pr')
 
-console.log("This is barney");
-console.log(context);
-
-
-setTimeout(function() {
-    console.log("In setTimeout");
-    request('GET /repos/:owner/:repo/pulls/:pull_number', context.repo({
-        pull_number: pull_number
-    })).then(function (pr_ressult) {
-        console.log(pr_result);
-        if (!pr_result.body) {
-            request('PATCH /repos/:owner/:repo/pulls/:pull_number/', context.repo({
-                pull_number: pull_number,
-                body: `![Barnard](${barney})`
-            }))
-        }
-    })
-    console.log("after setTimeout");
-}, 10000);
-
+Toolkit.run(async tools => {
+  const pr_result = await getPR(tools)
+  tools.log.debug("PR_BODY=" + pr_result.data.body);
+  if (!pr_result.data.body) {
+    await updatePR(tools);
+    tools.log.success('Barney has been applied.')
+  } else {
+    tools.log.success('No Barney needed.')
+  }
+}, {
+  event: ['pull_request.opened'],
+  secrets: ['GITHUB_TOKEN']
+})
