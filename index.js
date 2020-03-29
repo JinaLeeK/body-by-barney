982b71ebda8dbaa1c5bdb437b764392a780839f6
JinaLeeK <jina.lee@acquia.com>
2020-03-28 20:47:48 -0400

    fix context.

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const { Toolkit } = require('actions-toolkit')
 const { context, github: { request } } = new Toolkit()
-
-const prNumber = context.payload.pull_request.number;
+context.repo
+const pull_number = context.payload.pull_request.number;
 const barney = 'https://user-images.githubusercontent.com/1390106/66919899-3a406900-eff0-11e9-83ba-4e6c4c3dbfaf.jpg'
 
 console.log("This is barney");
 console.log(context);
 
+
 setTimeout(function() {
     console.log("In setTimeout");
-    request('GET /repos/:owner/:repo/pulls/:pull_number', context.repo({
-        pull_number: prNumber
-    })).then(function (prResult) {
-        console.log(prResult);
-        if (!prResult.body) {
-            request('PATCH /repos/:owner/:repo/pulls/:pull_number/', context.repo({
-                pull_number: prNumber,
+    request('GET /repos/:owner/:repo/pulls/:pull_number', context.payload.repository({
+        pull_number: pull_number
+    })).then(function (pr_ressult) {
+        console.log(pr_result);
+        if (!pr_result.body) {
+            request('PATCH /repos/:owner/:repo/pulls/:pull_number/', context.payload.repository({
+                pull_number: pull_number,
                 body: `![Barnard](${barney})`
             }))
         }
     })
     console.log("after setTimeout");
-
-    
-
 }, 10000);
 
